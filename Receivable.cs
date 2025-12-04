0fc84d6b810f1dd494f56949d4db9868f8c55f89
FedorFeofanov <fedor.feofanov@gmail.com>
2025-12-04 14:44:41 +0100

    Major classes added

---
+++ Receivable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetTracker
+{
+    public class Receivable
+    {
+        private string BorrowerName;
+        private DateTime ExpectedRepaymentDate;
+        private bool IsInterestAccruing;
+        private decimal RepaymentCollected;
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetTracker
+{
+    public abstract class Transaction
+    {
+        private int ID;
+        private double Amount;
+        private DateTime Date;
+        private string Description;
+        private string Category;
+        private string Currency;
+    }
+}
